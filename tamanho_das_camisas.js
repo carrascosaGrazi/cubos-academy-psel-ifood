a910f3cb25ca7eb9f5fe66f58c7d88c80231b502
GCarrascosa <45268990+GCarrascosa@users.noreply.github.com>
2021-08-13 21:06:38 -0300

    Outro afago no coração do Dev iniciante

---
+++ tamanho_das_camisas.js
@@ -0,0 +1,15 @@
+
+function solucao(tamanhoBr) {
+    if(tamanhoBr === "GG"){ 
+        console.log("XL");    
+       } else if(tamanhoBr === "P"){
+           console.log("S");
+       } else if (tamanhoBr  === "M"){
+           console.log("M");
+       } else if (tamanhoBr  === "G"){
+           console.log ("L");
+       }
+       else {
+           console.log("XS");
+       }
+   }
